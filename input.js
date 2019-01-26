151c111f34409bd078c4eb3b3a3a1eb903cd866c
Faith Christensen <faithchristensen111@gmail.com>
2019-01-25 21:40:36 -0800

    Started reverse input app

---
+++ input.js
@@ -0,0 +1,32 @@
+function inputDisplay() {
+    var inputText = document.getElementById("inputText").value;
+    inputText.addEventListener("input", inputText)
+};
+
+inputDisplay();
+
+//when typed, display text (object.addEventListener("input", myScript))
+//or <input oninput="myFunction(inputDisplay)>`
+
+
+
+
+
+
+
+
+
+
+
+
+// var inputDisplay = {
+//     letters: [],
+//     //     addText: function(inputText) {
+//     //         this.letters.push({
+//     //             inputText = inputText,
+//     //         })
+//     //         var inputText = document.getElementById("inputText").value;
+    
+//     //         console.log(letters);
+//     //     }
+// }
