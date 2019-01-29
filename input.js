533a09998e327515963a0cedbb771f5a0d120c97
Faith Christensen <faithchristensen111@gmail.com>
2019-01-28 17:19:56 -0800

    Implemented font styling, and created a function to reverse text upon input

--- input.js
+++ input.js
@@ -1,5 +1,13 @@
 function inputTest() {
     var inputText = document.getElementById("inputText").value;
     var showText = document.getElementById("textDisplay");
-    showText.innerText = inputText;
+    var reverseText = reverseString(inputText);
+    showText.innerText = reverseText;
+}
+
+function reverseString(str) {
+    var splitString = str.split('');
+    var reverseArray = splitString.reverse();
+    var joinArray = reverseArray.join('');
+    return joinArray;
 }
